96baed201f3790f3eab26ac68f4579ae9bffd9d6
Gridnev Nikita <gridnevnikit4@gmail.com>
2025-06-22 19:24:12 +0300

    fixed main.py fixed test_main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Any, List
+from typing import Any, List, Optional
 
 
 class Product:
@@ -15,18 +15,49 @@ class Product:
         """Инициализирует объект Product"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @classmethod
-    def new_product(cls, data: dict) -> "Product":
-        """Класс-метод, создающий объект Product из словаря."""
+    def new_product(cls, data: dict, products: Optional[List["Product"]] = None) -> "Product":
+        """Создаёт новый товар или обновляет существующий с таким же именем."""
+        if products is None:
+            products = []
+
+        for product in products:
+            if product.name == data['name']:
+                product.quantity += data['quantity']
+                if data['price'] > product.price:
+                    product.price = data['price']
+                return product
         return cls(
             name=data['name'],
             description=data['description'],
             price=data['price'],
             quantity=data['quantity']
         )
+
+    @property
+    def price(self) -> float:
+        """Выводит цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, value: float) -> None:
+        """Сеттер для ввода цены с проверкой"""
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif value < self.__price:
+            print(f"Вы пытаетесь понизить цену с {self.__price} до {value}.")
+            confirm = input("Вы уверены? (y/n): ")
+            if confirm.lower() == "y":
+                self.__price = value
+                print("Цена успешно обновлена.")
+            else:
+                print("Изменение цены отменено.")
+        else:
+            self.__price = value
+
 class Category:
     """Класс, представляющий категорию товаров"""
 
@@ -99,35 +130,35 @@ def create_objects_from_json(data: List[dict[str, Any]]) -> List[Category]:
 # print("Всего категорий:", Category.category_count)
 # print("Всего товаров:", Category.product_count)
 #
-if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
-    )
-
-    print(category1.products)
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(category1.product_count)
-
-    new_product = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5})
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    new_product.price = 800
-    print(new_product.price)
-
-    new_product.price = -100
-    print(new_product.price)
-    new_product.price = 0
-    print(new_product.price)
+# if __name__ == "__main__":
+#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+#     category1 = Category(
+#         "Смартфоны",
+#         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#         [product1, product2, product3]
+#     )
+#
+#     print(category1.products)
+#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+#     category1.add_product(product4)
+#     print(category1.products)
+#     print(category1.product_count)
+#
+#     new_product = Product.new_product(
+#         {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+#          "quantity": 5})
+#     print(new_product.name)
+#     print(new_product.description)
+#     print(new_product.price)
+#     print(new_product.quantity)
+#
+#     new_product.price = 800
+#     print(new_product.price)
+#
+#     new_product.price = -100
+#     print(new_product.price)
+#     new_product.price = 0
+#     print(new_product.price)
