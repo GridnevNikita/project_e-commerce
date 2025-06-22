dac53dfd2af0d9fc7136a9939ab72da2bdac649d
Gridnev Nikita <gridnevnikit4@gmail.com>
2025-06-22 17:57:14 +0300

    fixed main.py add property products in class Category and new_product in class Products

--- src/main.py
+++ src/main.py
@@ -18,7 +18,15 @@ class Product:
         self.price = price
         self.quantity = quantity
 
-
+    @classmethod
+    def new_product(cls, data: dict) -> "Product":
+        """Класс-метод, создающий объект Product из словаря."""
+        return cls(
+            name=data['name'],
+            description=data['description'],
+            price=data['price'],
+            quantity=data['quantity']
+        )
 class Category:
     """Класс, представляющий категорию товаров"""
 
@@ -40,6 +48,16 @@ class Category:
         """Добавляет продукт в приватный список"""
         self.__products.append(product)
         Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """Возвращает все товары в формате строки"""
+        lines = [
+            f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+            for product in self.__products
+        ]
+        return "\n".join(lines)
+
 
 def read_json_file(path: str) -> List[dict[str, Any]]:
     """Читает JSON-файл по указанному пути и возвращает данные в виде списка словарей"""
@@ -81,44 +99,35 @@ def create_objects_from_json(data: List[dict[str, Any]]) -> List[Category]:
 # print("Всего категорий:", Category.category_count)
 # print("Всего товаров:", Category.product_count)
 #
-# product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-# product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-# product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-#
-# print(product1.name)
-# print(product1.description)
-# print(product1.price)
-# print(product1.quantity)
-#
-# print(product2.name)
-# print(product2.description)
-# print(product2.price)
-# print(product2.quantity)
-#
-# print(product3.name)
-# print(product3.description)
-# print(product3.price)
-# print(product3.quantity)
-#
-# category1 = Category("Смартфоны",
-# "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-#                      [product1, product2, product3])
-#
-# print(category1.name == "Смартфоны")
-# print(category1.description)
-# print(len(category1.products))
-# print(category1.category_count)
-# print(category1.product_count)
-#
-# product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-# category2 = Category("Телевизоры",
-# "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-#                      [product4])
-#
-# print(category2.name)
-# print(category2.description)
-# print(len(category2.products))
-# print(category2.products)
-#
-# print(Category.category_count)
-# print(Category.product_count)
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3]
+    )
+
+    print(category1.products)
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    category1.add_product(product4)
+    print(category1.products)
+    print(category1.product_count)
+
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
